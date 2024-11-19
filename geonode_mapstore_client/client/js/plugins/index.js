b342bf09d3f5c6e82b3b5f309247f6af93711c5d
Maxime Audet-Roberge <maxime.audet-roberge@canada.ca>
2024-11-19 11:30:30 -0500

    Integrating the Shoreline Viewer plugin

--- geonode_mapstore_client/client/js/plugins/index.js
+++ geonode_mapstore_client/client/js/plugins/index.js
@@ -458,6 +458,10 @@ export const plugins = {
     LayerDetailViewerPlugin: toModulePlugin(
         'LayerDetailViewer',
         () => import(/* webpackChunkName: 'plugins/detail-viewer-plugin' */ '@js/plugins/LayerDetailViewer')
+    ),
+    ShorelineViewerPlugin: toModulePlugin(
+        'ShorelineViewer',
+        () => import(/* webpackChunkName: 'plugins/shoreline-viewer' */ '@js/plugins/ShorelineViewer')
     )
 };
 
